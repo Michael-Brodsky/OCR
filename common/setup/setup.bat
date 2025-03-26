3f9c2b552c5bb19a91374c65b81bf56252f6ddac
Michael Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2025-03-26 00:58:48 -0600

    Add files via upload

---
+++ common/setup/setup.bat
@@ -0,0 +1,92 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+:: PaperShredder setup script.
+::
+:: (c) 2025 Michael Brodsky
+:: mbrodskiis@gmail.com
+:: All rights reserved. Unauthorized use prohibited.
+::
+:: Creates the application filesystem install any 
+:: required dependencies. 
+::
+:: Parameters:
+::    (None)
+::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@echo off
+SETLOCAL EnableExtensions EnableDelayedExpansion
+SET PATH=%PATH%;C:\Windows\System32\
+SET Program=PaperShredder Setup
+SET Version=1.0
+SET SetupSubDir=setup
+SET InstallFile=install.bat
+SET LogFile=log.txt
+
+:: Application folders.
+SET AppFolders=tmp,catalog
+
+:: Application dependencies.
+SET dependencies[0].Name="GPL Ghostscript"
+SET dependencies[0].RegistryKey="HKEY_LOCAL_MACHINE\SOFTWARE\GPL Ghostscript\9.56.1"
+SET dependencies[0].InstallerUrl="https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9561"
+SET dependencies[0].FileName="gs9561w64.exe"
+SET dependencies[0].Args="/S"
+SET dependencies[1].Name="ImageMagick"
+SET dependencies[1].RegistryKey="HKEY_LOCAL_MACHINE\SOFTWARE\ImageMagick\Current"
+SET dependencies[1].InstallerUrl="https://imagemagick.org/archive/binaries"
+SET dependencies[1].FileName="ImageMagick-7.1.1-41-Q16-HDRI-x64-dll.exe"
+SET dependencies[1].Args="/VERYSILENT /NORESTART /FORCECLOSEAPPLICATIONS /LOADINF="imsetup.txt""
+SET dependencies[2].Name="OCR Tesseract"
+SET dependencies[2].RegistryKey="HKEY_LOCAL_MACHINE\SOFTWARE\Tesseract-OCR"
+SET dependencies[2].InstallerUrl="https://github.com/tesseract-ocr/tesseract/releases/download/5.5.0"
+SET dependencies[2].FileName="tesseract-ocr-w64-setup-5.5.0.20241111.exe"
+SET dependencies[2].Args="/S"
+
+SET /A sz_dependencies=2
+
+:MAIN
+::
+:: Create the application filesystem and install any
+:: required dependencies.
+::
+:: The setup root is the parent folder of this file.
+:: All other locations are subfolders thereof.
+::
+SET "RootFolder=%~dp0"
+SET "SetupFolder=%RootFolder%%SetupSubDir%"
+SET "Installer=%SetupFolder%\%InstallFile%"
+ECHO %Program% %Version%
+
+CALL :CREATE_FILESYSTEM "%RootFolder%" "%AppFolders%"
+
+:: Loop through each defined dependency and, if neccessary, 
+:: install it.
+FOR /L %%i IN (0 1 %sz_dependencies%) DO  ( 
+    CALL ECHO INSTALLING !dependencies[%%i].Name:"=!
+    CALL "%Installer%" %%dependencies[%%i].RegistryKey%% %%dependencies[%%i].InstallerUrl%% "%SetupFolder%" %%dependencies[%%i].FileName%% %%dependencies[%%i].Args%%
+    IF NOT ERRORLEVEL 1 (
+	CALL ECHO %%dependencies[%%i].Name%% installed successfully
+    ) ELSE (
+	CALL ECHO %%dependencies[%%i].Name%% install failed
+	GOTO MAIN_DONE
+    ) 
+)
+:MAIN_DONE
+EXIT /B %ERRORLEVEL%
+
+:CREATE_FILESYSTEM
+::
+:: Creates the given list of subfolders in the 
+:: specified root folder.
+::
+:: Parameters
+::    $1 - Root folder
+::    $2 - Comma-separated list of sub folders.
+::
+FOR %%x in (%~2) DO (
+    SET "folder=%~1%%x"
+    CALL ECHO CREATING !folder!
+    CALL MKDIR "!folder!" 
+)
+EXIT /B %ERRORLEVEL%
